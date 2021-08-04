ed6b0838514f9ecf79548aea4f06bf8b456877be
dkomarov <dmytro.komarov@davita.com>
2021-08-04 15:32:07 +0300

    init

---
+++ src/main/java/com/komarov/webschool/WebSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.komarov.webschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/komarov/webschool/WebSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komarov.webschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
