62b933aaaa9cce0f431b252fdb59e0db467265df
dkomarov <dmytro.komarov@davita.com>
2021-08-13 18:21:14 +0300

    The flow was changed. Fill database during the sql code.

--- src/main/java/com/komarov/webschool/configuration/SchoolConfiguration.java
+++ src/main/java/com/komarov/webschool/configuration/SchoolConfiguration.java
@@ -1,14 +1,13 @@
 package com.komarov.webschool.configuration;
 
-import com.komarov.webschool.entity.*;
-import com.komarov.webschool.repository.*;
+import com.komarov.webschool.entity.Teacher;
+import com.komarov.webschool.repository.TeacherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,33 +16,12 @@ import java.util.Optional;
 public class SchoolConfiguration {
 
     @Bean
-    public CommandLineRunner commandLineRunner(TeacherRepository teacherRepository,
-                                               SubjectRepository subjectRepository,
-                                               TeamRepository teamRepository,
-                                               StudentRepository studentRepository,
-                                               LessonRepository lessonRepository) {
+    public CommandLineRunner commandLineRunner(TeacherRepository teacherRepository) {
         return args -> {
-            Teacher arnold = new Teacher("arnold", "flannagan", "000000000");
-            Teacher debbie = new Teacher("debbie",  "adams", "000000000");
-            Teacher zhess = new Teacher("zhess", "black", "000000000");
-
-            Subject algebra = new Subject("algebra");
-            Subject geography = new Subject("geography");
-            Subject english = new Subject("english");
-
-            Team alfa = new Team("alfa");
-
-            Lesson lesson1 = new Lesson("topic1", Instant.now(), alfa, arnold, algebra);
-            Lesson lesson2 = new Lesson("topic2", Instant.now(), alfa, debbie, geography);
-            Lesson lesson3 = new Lesson("topic3", Instant.now(), alfa, zhess, english);
-            lessonRepository.saveAll(List.of(lesson1, lesson2, lesson3));
-
-            Student alex = new Student("alex","fitzgerald","000000000");
-            Student kate = new Student("kate","dodson","000000000");
-            Student robert = new Student("robert","finch","000000000");
-            studentRepository.saveAll(List.of(alex, kate, robert));
-
-            studentRepository.findAll().forEach(student -> studentRepository.updateGroupForStudent(student.getId(), alfa.getName()));
+            Teacher flannagan = new Teacher("arnold", "flannagan", "000000000");
+            Teacher adams = new Teacher("debbie", "adams", "000000000");
+            Teacher black = new Teacher("zhess", "black", "000000000");
+            teacherRepository.saveAll(List.of(flannagan, adams, black));
         };
     }
 
--- src/main/resources/sql/school.sql
+++ src/main/resources/sql/school.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS teachers
 (
-    id              BIGSERIAL PRIMARY KEY,
-    first_name      VARCHAR NOT NULL,
-    middle_name     VARCHAR,
-    last_name       VARCHAR NOT NULL,
-    patronymic_name VARCHAR,
-    phone_number    VARCHAR
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name  VARCHAR NOT NULL,
+    mobile     VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS subjects
@@ -14,7 +12,7 @@ CREATE TABLE IF NOT EXISTS subjects
     name VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS groups
+CREATE TABLE IF NOT EXISTS teams
 (
     id   BIGSERIAL PRIMARY KEY,
     name VARCHAR NOT NULL
@@ -22,16 +20,14 @@ CREATE TABLE IF NOT EXISTS groups
 
 CREATE TABLE IF NOT EXISTS students
 (
-    id              BIGSERIAL PRIMARY KEY,
-    first_name      VARCHAR NOT NULL,
-    middle_name     VARCHAR,
-    last_name       VARCHAR NOT NULL,
-    patronymic_name VARCHAR,
-    phone_number    VARCHAR,
-    group_id        BIGINT  NOT NULL,
-    CONSTRAINT group_fk
-        FOREIGN KEY (group_id)
-            REFERENCES groups (id)
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name  VARCHAR NOT NULL,
+    mobile     VARCHAR,
+    team_id    BIGINT,
+    CONSTRAINT team_fk
+        FOREIGN KEY (team_id)
+            REFERENCES teams (id)
             ON DELETE SET NULL
 );
 
@@ -40,12 +36,12 @@ CREATE TABLE IF NOT EXISTS lessons
     id         BIGSERIAL PRIMARY KEY,
     topic      VARCHAR   NOT NULL,
     date       TIMESTAMP NOT NULL,
-    group_id   BIGINT    NOT NULL,
+    team_id    BIGINT    NOT NULL,
     teacher_id BIGINT    NOT NULL,
     subject_id BIGINT    NOT NULL,
-    CONSTRAINT group_fk
-        FOREIGN KEY (group_id)
-            REFERENCES groups (id)
+    CONSTRAINT team_fk
+        FOREIGN KEY (team_id)
+            REFERENCES teams (id)
             ON DELETE SET NULL,
     CONSTRAINT teacher_fk
         FOREIGN KEY (teacher_id)
@@ -57,26 +53,11 @@ CREATE TABLE IF NOT EXISTS lessons
             ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS marks
-(
-    id         BIGSERIAL PRIMARY KEY,
-    mark       INT    NOT NULL,
-    student_id BIGINT NOT NULL,
-    lesson_id  BIGINT NOT NULL,
-    CONSTRAINT student_fk
-        FOREIGN KEY (student_id)
-            REFERENCES students (id)
-            ON DELETE SET NULL,
-    CONSTRAINT lesson_fk
-        FOREIGN KEY (lesson_id)
-            REFERENCES lessons (id)
-            ON DELETE SET NULL
-);
-
-CREATE TABLE IF NOT EXISTS absentees
+CREATE TABLE IF NOT EXISTS performances
 (
     id         BIGSERIAL PRIMARY KEY,
     is_present BOOLEAN NOT NULL,
+    mark       INT     NOT NULL,
     student_id BIGINT  NOT NULL,
     lesson_id  BIGINT  NOT NULL,
     CONSTRAINT student_fk
@@ -89,8 +70,52 @@ CREATE TABLE IF NOT EXISTS absentees
             ON DELETE SET NULL
 );
 
--- INSERT INTO teachers(first_name, middle_name, last_name, patronymic_name, phone_number)
--- VALUES ('', '', '', '', ''),
---        ('', '', '', '', ''),
---        ('', '', '', '', '');
+INSERT INTO teachers(first_name, last_name, mobile)
+VALUES ('arnold', 'flannagan', '000000000'),
+       ('debbie', 'adams', '000000000'),
+       ('zhess', 'black', '000000000');
+
+INSERT INTO subjects(name)
+VALUES ('algebra'),
+       ('geography'),
+       ('english');
+
+INSERT INTO teams(name)
+VALUES ('alfa');
+
+INSERT INTO students(first_name, last_name, mobile, team_id)
+VALUES ('alex', 'fitzgerald', '000000000', (SELECT id FROM teams WHERE name = 'alfa')),
+       ('kate', 'dodson', '000000000', (SELECT id FROM teams WHERE name = 'alfa')),
+       ('robert', 'finch', '000000000', (SELECT id FROM teams WHERE name = 'alfa'));
+
+INSERT INTO lessons(topic, date, team_id, teacher_id, subject_id)
+VALUES ('topic1',
+        TIMESTAMP '2011-05-16 15:36:38',
+        (SELECT id FROM teams WHERE name = 'alfa'),
+        (SELECT id FROM teachers WHERE last_name = 'flannagan'),
+        (SELECT id FROM subjects WHERE name = 'algebra')),
+       ('topic2',
+        TIMESTAMP '2011-05-16 17:36:38',
+        (SELECT id FROM teams WHERE name = 'alfa'),
+        (SELECT id FROM teachers WHERE last_name = 'adams'),
+        (SELECT id FROM subjects WHERE name = 'geography')),
+       ('topic3',
+        TIMESTAMP '2011-05-16 19:36:38',
+        (SELECT id FROM teams WHERE name = 'alfa'),
+        (SELECT id FROM teachers WHERE last_name = 'black'),
+        (SELECT id FROM subjects WHERE name = 'english'));
+
+INSERT INTO performances(is_present, mark, student_id, lesson_id)
+VALUES (true,
+        80,
+        (SELECT id FROM students WHERE last_name = 'fitzgerald'),
+        (SELECT id FROM lessons WHERE topic = 'topic1')),
+       (true,
+        85,
+        (SELECT id FROM students WHERE last_name = 'dodson'),
+        (SELECT id FROM lessons WHERE topic = 'topic2')),
+       (false,
+        0,
+        (SELECT id FROM students WHERE last_name = 'finch'),
+        (SELECT id FROM lessons WHERE topic = 'topic3'));
 
